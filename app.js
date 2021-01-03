516cd6bfb63d2d44b8ad6bca66198ad8a9967b78
Pranav <pranavmendi@gmail.com>
2021-01-03 06:08:11 +0530

    Setup google Auth 2.0

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const findOrCreate = require('mongoose-findorcreate')
 
 //Setting up the app-
 const app = express();
@@ -16,12 +18,24 @@ app.use(bodyParser.urlencoded({
     extended: true,
 }));
 app.use(session({
-    secret: 'keyboard cat',
+    secret: 'secret',
     resave: false,
     saveUninitialized: false,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 //Connecting to the DB-
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
@@ -33,6 +47,7 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 //Setting up our DB model-
 const User = new mongoose.model("User", userSchema);
